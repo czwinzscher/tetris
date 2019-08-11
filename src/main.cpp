d7e4e79c5133b5edf6448a5d4e2b1fbacdd0a3f1
Clemens Zwinzscher <czwinzscher2@gmail.com>
2019-08-11 14:28:54 +0200

    end ncurses and print score when game is finished

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,23 @@
+#include "tetris.hpp"
+
+#include <iostream>
+#include <ncurses.h>
+
 int main() {
+    // create Tetris Game
+    tetris::TetrisGame tg{22, 10};
+
+    // init ncurses
+    initscr();
+
+    // while (!tg.game_over()) {
+
+    // }
+
+    // end ncurses
+    endwin();
+
+    std::cout << "you finished the game with" << tg.score() << "points\n";
+
     return 0;
 }
