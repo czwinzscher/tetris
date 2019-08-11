755912d7e90f52662f35dc5ac4d04d5f3fa2a7e4
Clemens Zwinzscher <czwinzscher2@gmail.com>
2019-08-11 23:50:54 +0200

    implement next_piece

--- src/tetris.cpp
+++ src/tetris.cpp
@@ -1,23 +1,36 @@
 #include "tetris.hpp"
 
-using namespace tetris;
-
-Piece::Piece(Tetromino type, std::pair<int, int> loc, int ori)
+Piece::Piece(int type, location_t loc, int ori)
     : tet_type(type), location(loc), orientation(ori) {}
 
-TetrisGame::TetrisGame(int rows, int cols)
-    : rows(rows), cols(cols), cur_piece(next_piece()), cur_score(0),
-      mt(std::random_device{}()) {}
+TetrisGame::TetrisGame()
+    : cur_piece(next_piece()), cur_score(0), mt(std::random_device{}()) {}
 
 int TetrisGame::score() { return cur_score; }
 
-bool TetrisGame::next_state() { return !game_over(); }
+bool TetrisGame::next_state(Move m) {
+    switch (m) {
+        case Move::MOVE_LEFT: break;
+        case Move::MOVE_RIGHT: break;
+        case Move::MOVE_DOWN: break;
+        case Move::ROTATE_LEFT: break;
+        case Move::ROTATE_RIGHT: break;
+    }
+
+    return !game_over();
+}
+
+int TetrisGame::piece_at(int x, int y) const {
+    return 3;
+}
 
 Piece TetrisGame::next_piece() {
-    std::uniform_int_distribution<size_t> distr{0, 6};
+    std::uniform_int_distribution<int> distr{0, 6};
 
-    Tetromino t = all_tetrominos.at(distr(mt));
-    return Piece(t, {0, 3}, 0);
+    int n = distr(mt);
+    location_t l = start_positions.at(n);
+
+    return Piece(n, l, n * NUM_ORIENTATIONS);
 }
 
 bool TetrisGame::game_over() const { return false; }
