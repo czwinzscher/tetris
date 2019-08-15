380a3573ba2bb08a2d11ce8278e2dab2426f45df
Clemens <czwinzscher2@gmail.com>
2019-08-15 15:29:34 +0200

    add function definitions for lines, score, next and level windows

--- src/graphics.hpp
+++ src/graphics.hpp
@@ -5,6 +5,33 @@
 
 #include <ncurses.h>
 
+/**
+ * Initializes the colors for the different tetrominos using ncurses init_pair.
+ */
 void init_tetris_colors();
+
+/**
+ * Draws a box that shows the current playfield.
+ */
 void draw_board(WINDOW* w, TetrisGame tg);
+
+/**
+ * Draws a box that shows the number of total lines cleared.
+ */
+void draw_lines(WINDOW* w, TetrisGame tg);
+
+/**
+ * Draws a box that shows the current score.
+ */
+void draw_score(WINDOW* w, TetrisGame tg);
+
+/**
+ * Draws a box that shows the next piece.
+ */
+void draw_next(WINDOW* w, TetrisGame tg);
+
+/**
+ * Draws a box that shows the current level.
+ */
+void draw_level(WINDOW* w, TetrisGame tg);
 #endif
